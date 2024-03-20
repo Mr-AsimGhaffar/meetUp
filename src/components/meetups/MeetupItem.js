02bd3164dafd7b1d095edcd0772c600d16661cba
asim <asim.ghaffar71@gmail.com>
2024-03-20 15:50:22 +0500

    added list items

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,19 @@
+import classes from './MeetupList.module.css'
+function MeetupItem(props) {
+    return (<li>
+        <div>
+            <img src={props.image} alt={props.tittle} />
+        </div>
+        <div>
+            <h3>{props.tittle}</h3>
+            <address>{props.address}</address>
+            <p>{props.address}</p>
+        </div>
+        <div>
+            <button>To Favorites</button>
+        </div>
+  </li>
+  )
+}
+
+export default MeetupItem
